d7e961ba58f693f9174bab5ba9e5c69862266a50
mujn1461 <mjnlinda0527@gmail.com>
2024-02-08 10:50:50 -0600

    add event categories and save formats

--- event_code_gui.py
+++ event_code_gui.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 from tkinter import Text, Button, filedialog, messagebox
+import os
+import csv
 
 class TextCategorizerApp:
     def __init__(self, root):
@@ -11,7 +13,8 @@ class TextCategorizerApp:
         # add instructions text to top of GUI
         instruction_title = tk.Label(self.root, text="INSTRUCTIONS:")
         instruction_title.pack(pady=5, padx = 5, anchor="w", side="top", fill="x") 
-        instruction_label = tk.Label(self.root, text="Copy transcribed text into the box below. Highlight words or phrases and click a category button to assign to that category.\nCategories are mutually exclusive. To change a categorized word/phrase to another category, highlight the word/phrase and then click the new category.\nTo remove a category label from a word/phrase, highlight the word/phrase and click \'Clear\'.\nTo denote temporally nested events, use curly brackets around relevant words/phrases (e.g. \'{I get ready, so I brush my teeth and wash my face}\').\nWhen finished, click \'Save\' to output csv file with all word/phrases and their corresponding category codes. The input box will then clear and can be used again.", justify="left")
+        instruction_label = tk.Label(self.root, text="Copy transcribed text into the box below. Highlight words or phrases and click a category button to assign to that category.\nCategories are mutually exclusive. To change a categorized word/phrase to another category, highlight the word/phrase and then click the new category.\n\
+                                     To remove a category label from a word/phrase, highlight the word/phrase and click \'Clear\'.\nTo denote non-recall content, use curly brackets around relevant words/phrases (e.g. \'{It's an amusing story, all in all.}\').\nWhen finished, click \'Save\' to output csv file with all word/phrases and their corresponding category codes. The input box will then clear and can be used again.", justify="left")
         instruction_label.pack(pady=5, padx = 1, anchor="w", side="top", fill="x") 
         
         # create text box for transcribed recall to be pasted into
@@ -19,19 +22,47 @@ class TextCategorizerApp:
         self.text_area.pack(padx=10, pady=10, side="left")
 
         # categories - make sure name's align with colors assigned below in get_category_color
-        self.categories = ["Event", "Place", "Time", "Perceptual", "Emotion/Thought", "Conditional"]
+        self.categories_nar = ["Event-NAR", "Place-NAR", "Time-NAR", "Perceptual-NAR", "Emotion/Thought-NAR", "Semantic-NAR"]
+        self.categories_par = ["Emotion/Thought-PAR", "Semantic-PAR"]
+        self.categories_other=["Repetitions","Other"]
+        #self.categories = np.concatenate((self.categories_nar,self.categories_par,self.categories_other))
         
         # create category buttons
-        category_frame = tk.Frame(self.root)
-        category_frame.pack(side="left", padx=5)
+        category_frame_nar = tk.Frame(self.root)
+        category_frame_nar.pack(side="left", padx=5)
+        label_nar = tk.Label(category_frame_nar,text = "Narrator",font='Helvetica 18 bold')
+        label_nar.pack(anchor="w", side="top")
 
-        self.category_buttons = []
-        for category in self.categories:
+        self.category_buttons_nar = []
+        for category in self.categories_nar:
             color = self.get_category_color(category)
-            btn = Button(category_frame, text=category, command=lambda cat=category: self.assign_category(cat), borderwidth=3, relief="solid", bg=color, highlightbackground=color)
+            btn = Button(category_frame_nar, text=category, command=lambda cat=category: self.assign_category(cat), borderwidth=3, relief="solid", bg=color, highlightbackground=color)
             btn.pack(anchor="w", padx=5)
-            self.category_buttons.append(btn)
-    
+            self.category_buttons_nar.append(btn)
+
+        category_frame_par = tk.Frame(self.root)
+        category_frame_par.pack(side="left", padx=5)
+        label_par = tk.Label(category_frame_par,text = "Participant",font='Helvetica 18 bold')
+        label_par.pack(anchor="w", side="top")
+        self.category_buttons_par = []
+        for category in self.categories_par:
+            color = self.get_category_color(category)
+            btn = Button(category_frame_par, text=category, command=lambda cat=category: self.assign_category(cat), borderwidth=3, relief="solid", bg=color, highlightbackground=color)
+            btn.pack(anchor="w", padx=5)
+            self.category_buttons_par.append(btn)
+
+
+        category_frame_other = tk.Frame(self.root)
+        category_frame_other.pack(side="left", padx=5)
+        label_other = tk.Label(category_frame_other,text = "Other",font='Helvetica 18 bold')
+        label_other.pack(anchor="w", side="top")
+        self.category_buttons_other = []
+        for category in self.categories_other:
+            color = self.get_category_color(category)
+            btn = Button(category_frame_other, text=category, command=lambda cat=category: self.assign_category(cat), borderwidth=3, relief="solid", bg=color, highlightbackground=color)
+            btn.pack(anchor="w", padx=5)
+            self.category_buttons_other.append(btn)
+
         # add button to clear category assignment
         button_frame = tk.Frame(self.root)
         button_frame.pack(side="left", padx=5)
@@ -43,6 +74,7 @@ class TextCategorizerApp:
         submit_btn.pack(side = "top", padx=1)
 
         self.category_assignments = {}
+        #self.marked_indices = []
 
     # assigns selected word/phrase to category
     def assign_category(self, category):
@@ -54,10 +86,19 @@ class TextCategorizerApp:
             existing_category = self.category_assignments.get(selected_text)
             if existing_category:
                 self.text_area.tag_remove(existing_category, start, end)
-
+            # else:
+            #     selected_indices_range = list(range(start,end))
+            #     intersection = set(self.marked_indices)&set(selected_indices_range)
+            #     if intersection:
+            #         # todo: 
+            #         # stores correspondence between text, category, and marked indices (a list of text and indices as keys)
+            #         # search for the intersection in marked indices 
+            #         # change indices and marked text in category_assignments 
+            #         pass
             self.category_assignments[selected_text] = category
             self.text_area.tag_add(category, start, end)
             self.text_area.tag_config(category, background=self.get_category_color(category))
+            self.marked_indices.extend()
 
     # clears category from word/phrase so that there is no label
     def clear_category(self):
@@ -73,7 +114,8 @@ class TextCategorizerApp:
 
     # Assign unique hex colors for each category; aim to use pretty light colors so you can still read the text when it's highlighted. For more categories, can also add colors to actual text rather than highlighting
     def get_category_color(self, category):
-        colors = {"Event": "#f6fa82", "Place": "#b7ff87", "Time": "#bdd2ff", "Perceptual":"#ffc7f8", "Emotion/Thought":"#bdbdbd", "Conditional":"#ffc869"}
+        colors = {"Event-NAR": "#B65FCF", "Place-NAR": "#b7ff87", "Time-NAR": "#bdd2ff", "Perceptual-NAR":"#ffc7f8", "Emotion/Thought-NAR":"#F5B7B1",
+                   "Semantic-NAR":'#A3E4D7',"Emotion/Thought-PAR":'#F9E79F', "Semantic-PAR":'#E59866',"Repetitions":'#CCD1D1',"Other":'#95A5A6',"Conditional":"#ffc869"}
         return colors.get(category, "#FFFFFF")  # Default to white if category not found
     
     # save file to csv, with one column for selected text, and one column for corresponding category label
@@ -81,18 +123,30 @@ class TextCategorizerApp:
         filename = filedialog.asksaveasfilename(defaultextension=".csv", filetypes=[("CSV files", "*.csv")])
         if filename:
             with open(filename, "w") as file:
-                file.write("Word/Phrase,Category\n")
+                wr = csv.writer(file)
+                wr.writerow(['Utterance','Category'])
+                #file.write("Utterance,Category\n")
                 for word, category in self.category_assignments.items():
-                    file.write(f"{word},{category}\n")
-
+                    #file.write(f"{word},{category}\n")
+                    wr.writerow([str(word),str(category)])
+                
                 # after saving out text with categories, iterate through entire text to save each string within curly brackets as a separate entry
                 text_content = self.text_area.get("1.0", tk.END)
                 nested_texts = self.extract_nested_texts(text_content)
                 for nested_text in nested_texts:
-                    file.write(f"{nested_text},nested\n")
-
+                    #file.write(f"{nested_text},non-recall\n")
+                    wr.writerow([str(nested_text),'non-recall'])
+            file.close()
             messagebox.showinfo("Success", "CSV file saved successfully!")
-            
+
+            # save recall portions only
+            clean_recall = self.extract_recall_only(text_content)
+            save_dir = os.path.split(filename)[0]
+            story = os.path.split(filename)[1].split('.')[0]
+            clean_recall_filename = story+'_recall_only.txt'
+            with open(os.path.join(save_dir,clean_recall_filename),'w') as f:
+                f.write(clean_recall)
+            messagebox.showinfo("Success", "Clean recall saved successfully!")
             # clears text box for next transcript so that GUI can stay open/doesn't have to be re-loaded for each transcript
             self.clear_text_area()
 
@@ -108,7 +162,17 @@ class TextCategorizerApp:
                 break
         return nested_texts
     
-    # 
+    def extract_recall_only(self,text):
+        recall = ''
+        end_index = -1
+        start_index = text.find('{')
+        while start_index != -1:
+            recall+=text[end_index+1:start_index]
+            end_index = text.find('}', start_index)
+            start_index = text.find('{', end_index + 1)
+        recall+=text[end_index+1:]
+        return recall
+    
     def clear_text_area(self):
         self.text_area.delete("1.0", tk.END)
 
